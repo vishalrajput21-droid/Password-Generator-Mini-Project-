f4cccd320410cee03514c1ad05ac88a824103164
vishal Rajput <vishalrajput21082003@gmail.com>
2026-01-21 12:02:07 +0530

    logics is almost done

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const passGenBtn=document.querySelector('.pass-gen')
 const allCheckBox=document.querySelectorAll('input[type=checkbox]')
 const symbolsStr="`~!@#$%^&*(){}[]='./?<>,"
 let passWordLenght=10;
+let count=0;
 handleSlider();
 function handleSlider(){
     passLength.innerHTML=passWordLenght;    
@@ -20,7 +21,7 @@ slider.addEventListener('input',()=>{
     passWordLenght=slider.value;
     handleSlider();    
 });
-function randomInt(max,min){
+function randomInt(min,max){
   return Math.floor(Math.random()*(max-min+1))+min;
 };
 function upperletter(){
@@ -37,3 +38,76 @@ function randomsybols(){
      let randomIdex=Math.floor(Math.random()*symbolsStr.length);
      return symbolsStr[randomIdex];
 };
+function setIndicator(color){
+     strength.Style.backgroundColor=color;
+}
+
+function colourindicator(){
+    let selectUpper=false;
+    let selectLower=false;
+    let selectNumber=false;
+    let selectSybols=false;
+    if(upperCase.checked)selectUpper=true;
+    if(lowerCase.checked)selectLower=true;
+    if(numbers.checked)selectNumber=true;
+    if(symbols.checked)selectSybols=true;
+    if (selectUpper && selectLower && selectNumber && selectSybols){
+        setIndicator("#00ff00");
+    }
+    else if(selectUpper||selectLower||selectNumber||selectSybols){
+        setIndicator("#ff0000");
+    }
+    else if( (selectUpper||selectLower) && selectSybols&& selectNumber){
+        setIndicator("#0000ff");
+    }
+
+};
+
+function handleCheckbox(){
+    count=0;
+    allCheckBox.forEach((checkbox)=>{
+        if(checkbox.checked){
+            count++;
+        }
+        
+    })
+};
+allCheckBox.forEach((checkbox)=>{
+    checkbox.addEventListener("change",handleCheckbox);
+})
+ 
+
+passGenBtn.addEventListener("click",()=>{
+    if(count==0){
+        return;
+    } ;
+   let password=""
+ if( passWordLenght<count){
+            passWordLenght=count;
+            slider.value=passWordLenght;
+            handleSlider();
+        };
+    let funarr=[];
+    if (upperCase.checked){
+        funarr.push(upperletter);
+    }
+    if(lowerCase.checked)   { 
+        funarr.push(lowerletter);
+    }
+    if(numbers.checked){
+        funarr.push(randoNumber);
+    }
+    if(symbols.checked){
+        funarr.push(randomsybols);
+    } ;
+    for(let i=0; i<funarr.length;i++){
+        password+=funarr[i]();
+    };
+    for(let i = 0; i < passWordLenght; i++){
+    let randIndex = randomInt(0, funarr.length - 1);
+    password += funarr[randIndex]();
+}
+
+    passwordDisplay.value=password;
+
+})
